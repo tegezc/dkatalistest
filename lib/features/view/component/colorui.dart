2fa92fc7ed2a250b4ca6ea48679c27a1d2cd0b8d
tegezc <tegesitt@gmail.com>
2021-08-19 22:25:01 +0700

    color app

---
+++ lib/features/view/component/colorui.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ColorUI {
+  static Color mainColor() {
+    return Color.fromRGBO(64, 124, 236, 1);
+  }
+
+  static Color colorWhite() {
+    return Color.fromRGBO(249, 249, 249, 1);
+  }
+
+  static Color colorDisableButton() {
+    return Color.fromRGBO(124, 160, 228, 1);
+  }
+
+  static Color colorEnableButton() {
+    return Color.fromRGBO(44, 74, 173, 1);
+  }
+
+  static Color colorWeakPassword() {
+    return Color.fromRGBO(255, 152, 72, 1);
+  }
+
+  static Color colorFairPassword() {
+    return Color.fromRGBO(0, 176, 53, 1);
+  }
+
+  static Color colorStrongPassword() {
+    return Color.fromRGBO(0, 144, 42, 1);
+  }
+}
